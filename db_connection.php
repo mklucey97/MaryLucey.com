ade52100a7aceabae600b0441ebae25d0af99fe7
mklucey97 <mklucey97@gmail.com>
2024-12-15 18:29:40 -0600

    Fix connection to database 4413656_chatapp by correcting quotes and adding config

--- db_connection.php
+++ db_connection.php
@@ -1,7 +1,9 @@
 <?php
-$servername = "fdb1032.awardspace.net”;
+$servername = "fdb1032.awardspace.net";
 $username = "4413656_chatapp";
-$password = “PFct,ap#8e_p!Ci9”;
+$password = "PFct,ap#8e_p!Ci9";
+$database = "4413656_chatapp"; // Database name
+
 $conn = mysqli_connect($servername, $username, $password, $database);
 
 if (!$conn) {
