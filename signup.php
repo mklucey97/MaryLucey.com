8c5fec3e4f72f0e495aa1d276232b77a471df1c5
mklucey97 <mklucey97@gmail.com>
2025-02-09 19:50:12 -0600

    Ensure username, password, and email are provided before submission

--- signup.php
+++ signup.php
@@ -5,6 +5,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$username = $_POST['username'];
 	$password = $_POST['password'];
 	$email = $_POST['email'];
+	
+	if (!empty($username) && !empty($password) && !empty($email)) {
 
 
 
